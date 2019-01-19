4559cda1a37936fea3b91351269741ccd979a75b
Michael Albertz <michael@prio.one>
2019-01-19 20:19:17 +0100

    add babel and eslint

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-const fetch = require("node-fetch");
-const Progress = require("./src");
+const fetch = require('node-fetch')
+const Progress = require('./dist')
 
-const link = "http://ipv6.download.thinkbroadband.com/200MB.zip";
+const link = 'http://ipv6.download.thinkbroadband.com/200MB.zip'
 
-async function run() {
-  const response = await fetch(link);
-  const progress = new Progress(response, { throttle: 100 });
-  progress.on("progress", progress => {
+async function run () {
+  const response = await fetch(link)
+  const progress = new Progress(response, { throttle: 100 })
+  progress.on('progress', (p) => {
     process.stdout.write(
-      `${Math.floor(progress.progress * 100)}% - ${progress.doneh}/${
-        progress.totalh
-      } - ${progress.rateh} - ${progress.etah}                       \r`
-    );
-  });
-  const result = await response.text();
+      `${Math.floor(p.progress * 100)}% - ${p.doneh}/${p.totalh} - ${
+        p.rateh
+      } - ${p.etah}                       \r`
+    )
+  })
+  const result = await response.text()
   // console.log(result);
 }
 
-run();
+run()
